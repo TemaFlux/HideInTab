e222db938c6ce05e2b4f2c7cecf5e1b4ce090f18
TemaFlux <67328970+TemaFlux@users.noreply.github.com>
2021-02-20 23:38:18 +0200

    change to empty nick

--- src/me/temaflux/hit/Listeners.java
+++ src/me/temaflux/hit/Listeners.java
@@ -1,5 +1,6 @@
 package me.temaflux.hit;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,13 +8,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.EnumWrappers.PlayerInfoAction;
 import com.comphenix.protocol.wrappers.PlayerInfoData;
+import com.comphenix.protocol.wrappers.WrappedGameProfile;
 
 public class Listeners
 implements Listener {
+	public final ProtocolManager protocolmanager = ProtocolLibrary.getProtocolManager();
 	public final PacketAdapter packetadapter;
 	
 	public Listeners(JavaPlugin plugin) {
@@ -29,18 +35,36 @@ implements Listener {
     			Listeners.this.onPacketReceiving(event);
     		}
     	};
+    	protocolmanager.addPacketListener(this.packetadapter);
 	}
 	
-	public void onPacketSending(PacketEvent event) {
+	public void onPacketSending(PacketEvent e) {
 		try {
-			if (event.getPacket().getPlayerInfoAction().read(0) != PlayerInfoAction.ADD_PLAYER) return;
+			if (e.getPacket().getPlayerInfoAction().read(0) != PlayerInfoAction.ADD_PLAYER) return;
 		}
-		catch (Exception e) {
+		catch (Exception ex) {
 			return;
 		}
         
         List<PlayerInfoData> newPlayerInfoDataList = new ArrayList<PlayerInfoData>();
-        event.getPacket().getPlayerInfoDataLists().write(0, newPlayerInfoDataList);
+        for (PlayerInfoData x : e.getPacket().getPlayerInfoDataLists().getValues().get(0)) {
+        	if (x.getProfile().getUUID() == e.getPlayer().getUniqueId()) continue;
+        	
+        	//remove the player
+//        	(CraftPlayer) pl).getHandle().playerConnection.sendPacket(new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.REMOVE_PLAYER, ((CraftPlayer) p).getHandle()));
+        	PacketContainer packet = new PacketContainer(PacketType.Play.Server.PLAYER_INFO);
+        	packet.getPlayerInfoAction().write(0, PlayerInfoAction.REMOVE_PLAYER);
+        	try {
+				this.protocolmanager.sendServerPacket(e.getPlayer(), packet);
+			} catch (InvocationTargetException e1) {
+				e1.printStackTrace();
+			}
+        	
+        	WrappedGameProfile nev = new WrappedGameProfile(x.getProfile().getUUID(), "");
+        	nev.getProperties().putAll(x.getProfile().getProperties());
+        	newPlayerInfoDataList.add(new PlayerInfoData(nev, x.getLatency(), x.getGameMode(), null));
+        }
+        e.getPacket().getPlayerInfoDataLists().write(0, newPlayerInfoDataList);
 	}
 	
     public void onPacketReceiving(PacketEvent event) {

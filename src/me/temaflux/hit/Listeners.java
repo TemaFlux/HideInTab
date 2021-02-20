e7f5bd8bb01d3e1583dab6410d641e41e6bc74c7
TemaFlux <67328970+TemaFlux@users.noreply.github.com>
2021-02-20 22:50:54 +0200

    format

--- src/me/temaflux/hit/Listeners.java
+++ src/me/temaflux/hit/Listeners.java
@@ -7,7 +7,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.EnumWrappers.PlayerInfoAction;
@@ -15,24 +14,36 @@ import com.comphenix.protocol.wrappers.PlayerInfoData;
 
 public class Listeners
 implements Listener {
+	public final PacketAdapter packetadapter;
+	
 	public Listeners(JavaPlugin plugin) {
-		ProtocolLibrary.getProtocolManager().addPacketListener(
-    		new PacketAdapter(plugin, new PacketType[] {
-                PacketType.Play.Server.PLAYER_INFO
-            }) {
-    		
+    	this.packetadapter = new PacketAdapter(plugin, new PacketType[] {PacketType.Play.Server.PLAYER_INFO}) {
+    		// Redirect
     		@Override
     	    public void onPacketSending(PacketEvent event) {
-    			try {
-    				if (event.getPacket().getPlayerInfoAction().read(0) != PlayerInfoAction.ADD_PLAYER) return;
-    			}
-    			catch (Exception e) {
-    				return;
-    			}
-                
-                List<PlayerInfoData> newPlayerInfoDataList = new ArrayList<PlayerInfoData>();
-                event.getPacket().getPlayerInfoDataLists().write(0, newPlayerInfoDataList);
+    			Listeners.this.onPacketSending(event);
     	    }
-    	});
+    		// Redirect
+    		@Override
+    	    public void onPacketReceiving(PacketEvent event) {
+    			Listeners.this.onPacketReceiving(event);
+    		}
+    	};
+	}
+	
+	public void onPacketSending(PacketEvent event) {
+		try {
+			if (event.getPacket().getPlayerInfoAction().read(0) != PlayerInfoAction.ADD_PLAYER) return;
+		}
+		catch (Exception e) {
+			return;
+		}
+        
+        List<PlayerInfoData> newPlayerInfoDataList = new ArrayList<PlayerInfoData>();
+        event.getPacket().getPlayerInfoDataLists().write(0, newPlayerInfoDataList);
+	}
+	
+    public void onPacketReceiving(PacketEvent event) {
+		
 	}
 }

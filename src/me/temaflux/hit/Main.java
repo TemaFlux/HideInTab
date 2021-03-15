eccede752f1534cd536f0459da83f4e5bcdccc0c
TemaFlux <67328970+TemaFlux@users.noreply.github.com>
2021-03-15 10:49:58 +0200

    hide on tab | no hide player

--- src/me/temaflux/hit/Main.java
+++ src/me/temaflux/hit/Main.java
@@ -5,15 +5,11 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.temaflux.hit.nte.NametagManager;
-
 public class Main
 extends JavaPlugin
 implements Listener {
 	private static Main plugin = null;
 	private Listeners Listeners = null;
-//	private NTEListeners ntelisteners = null;
-	public NametagManager nmanager = null;
 	
 	@Override
     public void onLoad() {
@@ -23,10 +19,7 @@ implements Listener {
 	@Override
     public void onEnable() {
     	if (Bukkit.getPluginManager().getPlugin("ProtocolLib") != null) {
-    		this.nmanager = new NametagManager();
     		this.Listeners = new Listeners(this);
-//    		this.ntelisteners = new NTEListeners(this.nmanager);
-//    		Bukkit.getPluginManager().registerEvents(this.ntelisteners, this);
     		Bukkit.getPluginManager().registerEvents(this.Listeners, this);
     	}
     	else getLogger().warning("Плагин не будет работать если не установлен ProtocolLib!");
@@ -35,14 +28,9 @@ implements Listener {
 	@Override
     public void onDisable() {
 		if (this.Listeners != null) HandlerList.unregisterAll(Listeners);
-		nmanager.reset();
     }
 	
 	public static Main getPlugin() {
 		return plugin;
-	}
-
-	public void debug(String x) {
-		this.getLogger().info("[DEBUG] " + x);
 	}
 }
